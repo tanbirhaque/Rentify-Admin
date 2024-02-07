ca4eb1293a6ad08afa9b2047efe9d935f8462a3c
Tanbir Haque Anil <99077932+Anirat04@users.noreply.github.com>
2024-02-07 12:58:53 +0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AdminDash from "./AdminDash/AdminDash.jsx";
 import Register from "./Authentication/Register/Register.jsx";
 import Login from "./Authentication/Login/Login.jsx";
 import Reset from "./Authentication/Reset/Reset.jsx";
@@ -12,22 +11,6 @@ import { Toaster } from "react-hot-toast";
 import DashLayout from "./DashLayout/DashLayout.jsx";
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <AdminDash></AdminDash>,
-  // },
-  // {
-  //   path: "/register",
-  //   element: <Register />,
-  // },
-  // {
-  //   path: "/login",
-  //   element: <Login />,
-  // },
-  // {
-  //   path: "/reset",
-  //   element: <Reset />,
-  // },
   {
     path: "/",
     element: <Login />,
