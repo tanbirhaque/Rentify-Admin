e9f521c98f279f05acfc9e17b5d88ef42607a61e
Fahima <sfahima1388@gmail.com>
2024-02-10 09:13:53 +0600

    owner request card changed added by Fahima

--- src/DashLayout/User Manage/RoleChangeTable.jsx
+++ src/DashLayout/User Manage/RoleChangeTable.jsx
@@ -6,7 +6,6 @@ import Swal from "sweetalert2";
 const RoleChangeTable = ({ item, refetch }) => {
   const { _id, name, email, image, role } = item || {};
   const axiosSecure = useAxiosSecure();
-
   const handleMakeOwner = (id) => {
     // console.log(id);
     Swal.fire({
@@ -42,35 +41,67 @@ const RoleChangeTable = ({ item, refetch }) => {
       }
     });
   };
+  const handleMakeUser = (id) => {
+    Swal.fire({
+      position: "top",
+      icon: "success",
+      title: `Haven't added functionality yet!`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
   return (
     <>
-      <tr>
-        <td>
-          <div className="flex items-center gap-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-20 h-20">
-                <img src={image} alt="Avatar Tailwind CSS Component" />
-              </div>
+      {/* card format */}
+      {/* <div className="bg-[#f5f5f5] p-10 grid grid-rows-3"> */}
+      <div className="max-w-[360px] h-auto p-6 bg-white shadow-lg rounded-xl space-y-3">
+        <div className="flex gap-5">
+          <div className="avatar bg-white shadow-lg rounded-lg">
+            <div className="w-16">
+              <img src={image} className="p-1 rounded-xl" />
             </div>
           </div>
-        </td>
-        <td>{name} </td>
-        <td>{email}</td>
-        <td>{role}</td>
-        {/* <td>Pending</td> */}
-        <td>
-          {role === "Owner" ? (
-            <p className="text-green-700 text-xl">Owner</p>
-          ) : (
-            <button
-              onClick={() => handleMakeOwner(_id)}
-              className="btn btn-ghost bg-green-800 text-white btn-xs"
-            >
-              Make Owner
-            </button>
-          )}
-        </td>
-      </tr>
+          <div className="flex flex-col justify-center gap-1">
+            <p className="text-sm font-bold text-[#000000]">{name}</p>
+            <p className="text-[#3b4cb8] text-xs">Developer</p>
+          </div>
+        </div>
+        <p className="text-xs text-[#7e7e7e]">
+          Answering guest inquiries, directing phone calls, coordinating travel
+          plans, and more.
+        </p>
+        <div className="text-xs space-y-3 text-left text-[#000000] mt-3">
+          <p>
+            <span className="text-[#7e7e7e]">Email:</span> {email}
+          </p>
+          <hr />
+          <p>
+            <span className="text-[#7e7e7e]">Phone:</span> 1238545644
+          </p>
+          <hr />
+          <p>
+            <span className="text-[#7e7e7e]">Location:</span> Indonesia
+          </p>
+        </div>
+        {role === "Owner" ? (
+          <button
+            onClick={() => handleMakeUser(_id)}
+            className="btn btn-sm rounded-lg font-normal bg-[#ff6746] text-white text-xs hover:bg-white hover:text-[#ff6746] hover:border-[#ff6746]"
+            style={{ padding: "5.5px 14px" }}
+          >
+            Remove from Owner
+          </button>
+        ) : (
+          <button
+            onClick={() => handleMakeOwner(_id)}
+            className="btn btn-sm rounded-lg font-normal bg-[#4ba7dc] text-white text-xs hover:bg-white hover:text-[#4ba7dc] hover:border-[#4ba7dc]"
+            style={{ padding: "5.5px 14px" }}
+          >
+            Make Owner
+          </button>
+        )}
+      </div>
+      {/* </div> */}
     </>
   );
 };
