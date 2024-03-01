0e1255c18bb1c661b1b8f86b4000d189781c3764
Tanbir Haque <tanbirhaque53@gmail.com>
2024-03-01 19:06:54 +0600

    Sidebar updated code

--- src/DashLayout/DashLayout.jsx
+++ src/DashLayout/DashLayout.jsx
@@ -18,16 +18,19 @@ const DashLayout = () => {
       <div className="flex">
         {/* Sidebar of dashboard starts here */}
         <div
-          className={`side_bar bg-[#0f172a] min-h-screen fixed w-64  text-white transition-all duration-300 ${isSidebarOpen ? "" : "-ml-64"
+          className={`side_bar bg-[#0f172a] min-h-screen fixed w-64 xl:min-w-[300px] z-[99999] text-white transition-all duration-300 ${isSidebarOpen ? "-ml-64 lg:ml-0" : "lg:-ml-64 xl:-ml-[300px]"
             }`}
         >
           <SideBar></SideBar>
         </div>
         {/* Main content of Dashboard starts here */}
-        <div className={`main_content w-svw transition-all duration-300 ${isSidebarOpen ? "ml-64" : ""}`}>
+        <div className={`main_content w-svw transition-all duration-300 ${isSidebarOpen ? "lg:ml-64 xl:ml-[300px]" : ""}`}>
           <DashNav toggleSidebar={toggleSidebar} ></DashNav>
           <div className="flex-1">
             <Outlet></Outlet>
+            {/* <div className="fixed z-[99999] right-8 bottom-12">
+              <ToTop></ToTop>
+            </div> */}
           </div>
         </div>
       </div>
